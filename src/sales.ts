47ce3eead3725d4e08abd4a1497065bd5dd207e3
dblock <dblock@dblock.org>
2021-08-08 10:36:47 -0400

    Added workflow to automatically make pull requests with updates.

--- src/sales.ts
+++ src/sales.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash';
-import * as fs from 'fs';
+import * as fs from 'async-file';
 import * as moment from 'moment';
 
 // 300 AI Generated Nude Portrait #7 tokens start at TokenId=191
@@ -15,37 +15,53 @@ function toETH(amount) {
   return (amount / 1000000000000000000).toFixed(3);
 }
 
-async function main() {
-  try {
-    
-    for(var frameIndex = 1; frameIndex <= 300; frameIndex++) {
-      const filename = 'data/ai-generated-nude-portraits-7/' + frameIndex + '.json';
-      var data = { logs: [] };
-      if (fs.existsSync(filename)) {
-        data = JSON.parse(Buffer.from(await fs.readFileSync(filename)).toString());
-      }
+async function sales() {
+  var results = []
+  for(var frameIndex = 1; frameIndex <= 300; frameIndex++) {
+    const filename = 'data/ai-generated-nude-portraits-7/' + frameIndex + '.json';
+    var data = { logs: [] };
+    if (fs.exists(filename)) {
+      data = JSON.parse(Buffer.from(await fs.readFile(filename)).toString());
+    }
 
-      var first = true;
-      for(const log of _.reverse(data.logs)) {
-        var dt = moment.unix(parseInt(log.timeStamp, 16));
-        if (log.method == 'acceptBid' || log.method == 'buy') {
-          var amount = toETH(parseInt(log.data));
-          if (first) {
-            console.log("frame " + frameIndex + " sold for " + amount + " ETH on " + dt.toString() + " | " + frameUrl(frameIndex));
-            first = false;
-          } else {
-            console.log("  sold for " + amount + " ETH on " + dt.toString());
-          }
-        } else if (log.method == 'setSalePrice') {
-          if (first) {
-            var amount = toETH(log.amount);
-            console.log("frame " + frameIndex + " was listed for sale for " + amount + " ETH on " + dt.toString() + " | " + frameUrl(frameIndex));
-            first = false;
-          }
+    var first = true;
+    for(const log of _.reverse(data.logs)) {
+      var dt = moment.unix(parseInt(log.timeStamp, 16));
+      if (log.method == 'acceptBid' || log.method == 'buy') {
+        var amount = toETH(parseInt(log.data));
+        if (first) {
+          results.push("frame " + frameIndex + " sold for " + amount + " ETH on " + dt.toString() + " | " + frameUrl(frameIndex));
+          first = false;
+        } else {
+          results.push("  sold for " + amount + " ETH on " + dt.toString());
+        }
+      } else if (log.method == 'setSalePrice') {
+        if (first) {
+          var amount = toETH(log.amount);
+          results.push("frame " + frameIndex + " was listed for sale for " + amount + " ETH on " + dt.toString() + " | " + frameUrl(frameIndex));
+          first = false;
         }
       }
     }
+  }
+  return results;
+}
 
+async function updateREADME(results) {
+  var data = await fs.readFile('README.md', 'utf8');
+  var result = data.replace(/```\nnpm run sales\n[\s\S.]*```/m, '```\nnpm run sales\n\n' + results.join("\n") + "\n```");
+  await fs.writeFile('README.md', result, 'utf8');
+}
+
+async function main() {
+  try {
+    var results = await sales();
+
+    await updateREADME(results);
+
+    _.each(results, (result) => {
+      console.log(result)
+    });
   } catch(error) {
     console.log(error)
   }
